468e643bcf3dd0767444ddc842cc6e36ef2c9ebc
sharon_ob23 <sharon@sharonobrien.com>
2023-01-13 22:24:16 -0500

    middle of firebase

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,18 +1,29 @@
-import { signInWithGooglePopup,createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils" 
-
-
+import { getRedirectResult } from "firebase/auth";
+import { useEffect } from "react";
+import { 
+    auth,
+    signInWithGooglePopup, 
+    //signInWithGoogleRedirect, 
+    createUserDocumentFromAuth 
+} from "../../utils/firebase/firebase.utils" 
 const SignIn = () => {
+    /*useEffect( async () => {
+        const response = await getRedirectResult(auth);
+        if(response){
+            const userDocRef = await createUserDocumentFromAuth(response.user); 
+        }
+    },[]);*/
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
-    }
-    
+        console.log(userDocRef);
+    };
+   
     return(
         <div>
         <h1>Sign In Page</h1>
-        <button onClick={logGoogleUser}>
-            Sign in with Google Popup
-        </button>
+        <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+        {/*<button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>*/}
         </div>
     )
 }
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
-//import firebase from 'firebase/app';
+import {getRedirectResult} from 'firebase/auth';
 import {
+  
 getAuth,
 signInWithRedirect,
 signInWithPopup,
@@ -16,25 +17,17 @@ import {
 const firebaseConfig = {
 
     apiKey: "AIzaSyANqHRAwsCjwUEG4fAFg6CMgTRuktKfZEM",
-  
     authDomain: "crown-db-791ff.firebaseapp.com",
-  
     projectId: "crown-db-791ff",
-  
     storageBucket: "crown-db-791ff.appspot.com",
-  
     messagingSenderId: "420780727381",
-  
     appId: "1:420780727381:web:77fb0d93c8ed0ad98888dc"
-  
   };
-  
-
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
-provider.setCustomParameters({
+googleProvider.setCustomParameters({
 prompt: 'select_account',
 });
 
@@ -43,14 +36,16 @@ if (!userAuth) return;
   console.log(userAuth);
 };
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
 export const db = getFirestore();//access db
+
 export const createUserDocumentFromAuth = async (userAuth) => {
  const userDocRef = doc(db, 'users', userAuth.uid);
+ console.log(userDocRef);
+ 
  const userSnapshot = await getDoc(userDocRef);
-
  if(!userSnapshot.exists()){
-  const { displayName,email } = userAuth;
+  const { displayName, email } = userAuth;
   const createdAt = new Date();
 
   try {
@@ -63,6 +58,7 @@ export const createUserDocumentFromAuth = async (userAuth) => {
     console.log('error creating the user', error.message);
   }
 }
- return(userDocRef);
+return userDocRef;
+ console.log(userSnapshot);
 
 };
