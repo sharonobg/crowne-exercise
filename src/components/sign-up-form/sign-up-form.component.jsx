c2e1306051e06b1b5b283339f8616fb40d4ac586
sharon_ob23 <sharon@sharonobrien.com>
2023-02-08 22:23:24 -0500

    added buttons and styles and classnames types object

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,7 +1,8 @@
 import{useState} from 'react';
 import  FormInput from '../form-input/form-input.component';
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
-
+import './sign-up-form.styles.scss';
+import Button from '../button/button.component';
 const defaultFormFields = {
     displayName: '',
     email: '',
@@ -43,8 +44,9 @@ const SignUpForm = () => {
         setFormFields({...formFields,[name]: value})
     }
     return(
-        <div>
-            <h1>Sign up with your email and password</h1>
+        <div className='sign-up-form-container'>
+            <h2>Don't have an account?</h2>
+            <span >Sign up with your email and password</span>
             <form onSubmit={handleSubmit}>
                 <FormInput 
                     label="Display Name"
@@ -78,7 +80,7 @@ const SignUpForm = () => {
                     name="confirmPassword" 
                     value={confirmPassword} 
                 />
-                <button type="submit">Sign Up</button>
+                <Button type="submit">Sign Up</Button>
             </form>
         </div>
     )
